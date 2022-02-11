9121e98ff5588a0e112b21236768e2e51e371f29
Nirakar <nirakar5656jad@gmail.com>
2022-02-11 12:45:57 +1100

    #1 - installing cypress test.

---
+++ cypress-teamcity-learn/cypress/integration/app.spec.ts
@@ -0,0 +1,10 @@
+import * as cypress from "cypress";
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200')
+  })
+
+  it('displays two todo items by default', () => {
+    cy.get('div.toolbar').should('contain', 'Welcome');
+  });
+});
